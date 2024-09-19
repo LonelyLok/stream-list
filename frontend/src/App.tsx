c3a12908259ccf2c0fe6104589a1e2cae0192408
Hei Lok Yu <lonelylok101@gmail.com>
2024-09-19 11:27:07 -0700

    add github source link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,9 @@ import {
   Chip,
   Stack,
   Avatar,
+  CircularProgress,
+  Tooltip,
+  Button,
 } from '@mui/joy';
 
 const backendUrl = 'https://stream-list-backend.onrender.com/upcoming_streams';
@@ -22,6 +25,7 @@ type Video = {
   thumbnails: Record<string, any>;
   title: string;
   liveBroadcastContent?: string;
+  streamer?: string;
 };
 
 type Data = {
@@ -33,13 +37,18 @@ type Data = {
 
 function App() {
   const [videos, setVideos] = useState<Video[]>([]);
-  const [streamers, setStreamers] = useState<Omit<Data,'videos'>[]>([]);
+  const [streamers, setStreamers] = useState<Omit<Data, 'videos'>[]>([]);
+  const [loading, setLoading] = useState(true);
   const getData = async () => {
     try {
       const response = await fetch(`${backendUrl}`);
       const data: Record<string, Data> = await response.json();
+      // await new Promise((resolve) => setTimeout(resolve, 2000));
       // const data: Record<string, Data> = testData;
-      const videos = Object.values(data).flatMap((d) => d.videos);
+      const videos = Object.values(data).flatMap((d) => {
+        return d.videos.map((v) => ({ ...v, streamer: d.name }));
+      });
+      setLoading(false);
       setStreamers(
         Object.values(data).map((d) => {
           return {
@@ -70,78 +79,129 @@ function App() {
         sx={{ color: 'white', mb: 2, alignSelf: 'center' }}
       >
         Upcoming Streams
+        <Tooltip title={'View source code on GitHub'}>
+          <Button
+            variant='plain'
+            component='a'
+            href='https://github.com/LonelyLok/stream-list'
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              ml: 1,
+              '&:hover': {
+                backgroundColor: 'rgba(255, 255, 255, 0.1)', // Light white for hover effect
+              },
+            }}
+          >
+            <img
+              style={{ width: 24, height: 24 }}
+              src={
+                'https://github.com/LonelyLok/LonelyLok.github.io/blob/master/src/assets/github-mark-white.png?raw=true'
+              }
+              alt='github'
+            />
+          </Button>
+        </Tooltip>
       </Typography>
-      <Box sx={{ width: '100%', maxWidth: '1500px', margin: '0 auto', mb: 3 }}>
-        <Stack
-          direction='row'
-          spacing={1}
+      {loading ? (
+        <Box
           sx={{
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            gap: 1,
-            mb: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
           }}
         >
-          {streamers.map((streamer, index) => (
-            <Avatar
-              key={index}
-              src={streamer.iconURL}
-              alt={`Streamer ${index + 1}`}
-              sx={{ width: 80, height: 80 }}
-            />
-          ))}
-        </Stack>
-      </Box>
-      <Box sx={{ width: '100%', maxWidth: '1500px', margin: '0 auto' }}>
-        <Grid container spacing={3} sx={{ flexGrow: 1 }}>
-          {videos.map((video, index) => (
-            <Grid xs={12} sm={3} md={4} lg={3} key={index}>
-              <Card variant='outlined' sx={{ height: 240, width: 320, p: 1 }}>
-                <AspectRatio ratio='2'>
-                  <CardOverflow>
-                    <Link
-                      href={`https://www.youtube.com/watch?v=${video.id}`}
-                      overlay
-                      underline='none'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                    >
-                      <img
-                        src={video.thumbnails.medium.url}
-                        height={video.thumbnails.medium.height}
-                        width={video.thumbnails.medium.width}
-                      />
-                    </Link>
-                  </CardOverflow>
-                </AspectRatio>
-                <CardContent>
-                  <Typography level='title-lg' noWrap={true}>
-                    {video.title}
-                  </Typography>
-                  <Typography level='body-sm'>
-                    Time: {new Date(video.scheduledStartTime).toLocaleString()}
-                  </Typography>
-                  <Typography level='body-sm'>
-                    Status:
-                    <Chip
-                      size='sm'
-                      variant='solid'
-                      color={
-                        video?.liveBroadcastContent === 'live'
-                          ? 'danger'
-                          : 'neutral'
-                      }
-                      sx={{ marginLeft: '8px' }}
-                    >
-                      {video?.liveBroadcastContent?.toUpperCase()}
-                    </Chip>
-                  </Typography>
-                </CardContent>
-              </Card>
+          <CircularProgress />
+          <Typography level='body-sm' sx={{ mt: 1, color: 'white' }}>
+            Can take a up to a minute to load
+          </Typography>
+        </Box>
+      ) : (
+        <div>
+          <Box
+            sx={{ width: '100%', maxWidth: '1500px', margin: '0 auto', mb: 3 }}
+          >
+            <Stack
+              direction='row'
+              spacing={1}
+              sx={{
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                gap: 1,
+                mb: 2,
+              }}
+            >
+              {streamers.map((streamer, index) => (
+                <Tooltip title={streamer.name}>
+                  <Avatar
+                    key={index}
+                    src={streamer.iconURL}
+                    alt={`Streamer ${index + 1}`}
+                    sx={{ width: 80, height: 80 }}
+                  />
+                </Tooltip>
+              ))}
+            </Stack>
+          </Box>
+          <Box sx={{ width: '100%', maxWidth: '1500px', margin: '0 auto' }}>
+            <Grid container spacing={3} sx={{ flexGrow: 1 }}>
+              {videos.map((video, index) => (
+                <Grid xs={12} sm={3} md={4} lg={3} key={index}>
+                  <Card
+                    variant='outlined'
+                    sx={{ height: 270, width: 320, p: 1 }}
+                  >
+                    <AspectRatio ratio='2'>
+                      <CardOverflow>
+                        <Link
+                          href={`https://www.youtube.com/watch?v=${video.id}`}
+                          overlay
+                          underline='none'
+                          target='_blank'
+                          rel='noopener noreferrer'
+                        >
+                          <img
+                            src={video.thumbnails.medium.url}
+                            height={video.thumbnails.medium.height}
+                            width={video.thumbnails.medium.width}
+                          />
+                        </Link>
+                      </CardOverflow>
+                    </AspectRatio>
+                    <CardContent>
+                      <Typography level='title-lg' noWrap={true}>
+                        {video.title}
+                      </Typography>
+                      <Typography level='body-sm' noWrap={true}>
+                        Streamer: {video?.streamer}
+                      </Typography>
+                      <Typography level='body-sm'>
+                        Time:{' '}
+                        {new Date(video.scheduledStartTime).toLocaleString()}
+                      </Typography>
+                      <Typography level='body-sm'>
+                        Status:
+                        <Chip
+                          size='sm'
+                          variant='solid'
+                          color={
+                            video?.liveBroadcastContent === 'live'
+                              ? 'danger'
+                              : 'neutral'
+                          }
+                          sx={{ marginLeft: '8px' }}
+                        >
+                          {video?.liveBroadcastContent?.toUpperCase()}
+                        </Chip>
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
-      </Box>
+          </Box>
+        </div>
+      )}
     </Box>
   );
 }
