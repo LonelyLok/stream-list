1bf222ac1159fb7c1f3c1d8e4de5ec4bbb92d4d1
LonelyLok <40349145+LonelyLok@users.noreply.github.com>
2026-05-15 07:08:39 -0700

    debug/missing-video (#11)

--- backend/api/api_test.go
+++ backend/api/api_test.go
@@ -10,3 +10,16 @@ func TestGetAllUpcomingStreams(t *testing.T) {
 	util.EnvSetUp()
 	TestFun()
 }
+
+func TestScrapeStreams(t *testing.T) {
+	videos, err := scrapeStreams("UCgnfPPb9JI3e9A4cXHnWbyg") // Mori Calliope
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("found %d streams", len(videos))
+
+	for _, v := range videos {
+		t.Logf("%s | %s | %s | %s", v.ID, v.LiveBroadcastContent, v.ScheduledStartTime, v.Title)
+	}
+}
